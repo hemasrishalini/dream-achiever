bc3d0bedcc1a4619fec02fc9a71c297a06b75bdf
hemasrishalini <hemasrishalini@gmail.com>
2023-01-05 20:08:43 +0530

    prime

---
+++ dream-achiever/src/java_simple_programs/PrimeNumber1ToN.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java_simple_programs;
+
+/**
+ *
+ * @author hemas
+ */
+public class PrimeNumber1ToN {
+    public static void main(String[] args){
+        primeNumber(72);
+        
+    }
+    public static void primeNumber(int num){
+        boolean isprime = true;
+        for(int i = 2; i<(num/2);i++){
+            if(num%2==0){
+                isprime = false;
+                break;
+            }
+        }
+        if(isprime==true){
+            System.out.println(num+" is a prime number");
+        }else{
+            System.out.println(num+" is not a prime number");
+        }
+        
+    }
+    
+}
